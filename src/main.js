9dd8faeda2afe93371e5b4f71df7fbb8b18c27e8
Todd Pickell <tapickell@gmail.com>
2016-11-14 23:03:07 -0600

    adds initial html css and js for view

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-// put al the codes in here
+const electron = require("electron")
+
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+app.on('ready', _ => {
+  mainWindow = new BrowserWindow({
+    height: 400,
+    width: 400
+  })
+
+  mainWindow.loadURL(`file://${__dirname}/countdown.html`)
+
+  mainWindow.on('close', _ => {
+    mainWindow = null
+  })
+})
